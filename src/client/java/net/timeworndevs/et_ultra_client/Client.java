56694b7f6b74f136407a57b0af9ef52139247ade
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2024-03-22 12:02:42 -0500

    Initial commit

---
+++ src/client/java/net/timeworndevs/et_ultra_client/Client.java
@@ -0,0 +1,10 @@
+package net.timeworndevs.et_ultra_client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
